656fe7bb9a8b43f67b8a071ad9b66da6975c02dd
Gurney Buchanan <Gurney Buchanan>
2016-03-02 15:24:16 -0500

    Initial commit

---
+++ app/src/main/java/edubuchanangb/appstate/cs/student/basic_display/Basic_Display_Window.java
@@ -0,0 +1,13 @@
+package edubuchanangb.appstate.cs.student.basic_display;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Basic_Display_Window extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic__display__window);
+    }
+}
